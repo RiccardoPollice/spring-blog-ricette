7bfa4b78e9d312da2f3eb5fd4c838de059c22ced
RiccardoPollice <riccardo.pollice17@gmail.com>
2024-01-22 14:44:40 +0100

    Fine Milestone 1 e Milestone 2

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,6 @@
 INSERT INTO recipes (portions, preparation_time, description, title, url, ingredients) VALUES(5, 150, 'È un dessert al cucchiaio a base di savoiardi inzuppati nel caffè e ricoperti di una crema, composta di mascarpone, uova e zucchero', 'tiramisù', 'https://www.lemillericette.it/wp-content/uploads/2020/11/TIRAMISU-RICETTA-CLASSICA-con-laggiunta-di-un-INGREDIENTE-GOLOSO.jpeg','Mascarpone 750 gr,Uova 5,Savoiardi 250 gr,Zucchero 120 gr,Caffè, Cacao amaro');
-INSERT INTO category (name) VALUES('dolci')
+INSERT INTO categorie(name)VALUES('Antipasto');
+INSERT INTO categorie(name)VALUES('Primo Piatto');
+INSERT INTO categorie(name)VALUES('Secondo Piatto');
+INSERT INTO categorie(name)VALUES('Contorno');
+INSERT INTO category (name) VALUES('dolci');
