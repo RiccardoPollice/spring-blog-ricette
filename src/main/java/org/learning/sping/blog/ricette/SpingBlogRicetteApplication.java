235a2fdbc61bd47ab685b10781947defac9299d1
RiccardoPollice <riccardo.pollice17@gmail.com>
2024-01-22 09:30:50 +0100

    Struttura del progetto

---
+++ src/main/java/org/learning/sping/blog/ricette/SpingBlogRicetteApplication.java
@@ -0,0 +1,13 @@
+package org.learning.sping.blog.ricette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingBlogRicetteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingBlogRicetteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/learning/sping/blog/ricette/SpingBlogRicetteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.learning.sping.blog.ricette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingBlogRicetteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
